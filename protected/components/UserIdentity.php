338bf6f1c60c5d5475bc2d5a6042ef9c2506bd41
kpp <humbug@deeptown.org>
2012-01-29 06:29:06 +0400

    user data protetion + /api/gb/get/ID added

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -7,7 +7,7 @@ class UserIdentity extends CUserIdentity
     public function authenticate()
     {
         $username=strtolower($this->username);
-        $user=User::model()->find('LOWER(username)=?',array($username));
+        $user=User::model()->private_data()->find('LOWER(username)=?',array($username));
         if($user===null)
             $this->errorCode=self::ERROR_USERNAME_INVALID;
         else if(!$user->validatePassword($this->password))
